cfcd0f82fdaf7cbaf3346a37f069a8eaf36e3c78
LucasRodrigues <lucasrodriguesbr12@gmail.com>
2022-11-28 01:39:17 -0300

    ByteBank

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,159 @@
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Util;
+using bytebank_ATENDIMENTO.ByteBank.Exceptions;
+using bytebank_ATENDIMENTO.ByteBankAtendimento;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Net.Http.Headers;
+#region Exemplos de arrays
+int[] idade = new int[5];
+
+//esse array é basicamente um vetor de 5 posições, entretando possui alguns metodos como Ordenar
+//existe essas duas formas de fazer:
+//Array array = Array.CreateInstance(typeof(int),5);
+//Array aa = new double[5];
+//AtribuindoValores(idade);
+//Getvalores(idade);
+//Console.WriteLine("A media é: "+MediaIdade(idade));
+//TestaBuscarPalavra();
+//TestaArrayDeContasCorrents();
+
+
+
+
+
+
+
+
+
+
+
+
+void AtribuindoValores(int[] idade)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        idade[i] = i + 1;
+    }
+}
+void Getvalores(int[] idade)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Idade no indice {i}: {idade[i]}");
+    }
+    AtribuindoValores(idade);
+
+    Console.ReadKey();
+
+
+
+
+}
+double MediaIdade(int[] idade)
+{
+    double acumulador = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        acumulador += idade[i];
+    }
+    return acumulador / 5;
+}
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite a {i + 1}º palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine("A palavra é: " + busca);
+            break;
+        }
+
+    }
+
+
+
+}
+void TestaArrayDeContasCorrents()
+{
+    ListaDeContasCorrentes listaDeContas = new ListaDeContasCorrentes();
+    {
+        listaDeContas.Adicionar(new ContaCorrente(874, "5679787-A"));
+        listaDeContas.Adicionar(new ContaCorrente(884, "4456668-B"));
+        listaDeContas.Adicionar(new ContaCorrente(147, "174785-C"));
+        listaDeContas.Adicionar(new ContaCorrente(874, "5679787-D"));
+        listaDeContas.Adicionar(new ContaCorrente(884, "4456668-E"));
+        listaDeContas.Adicionar(new ContaCorrente(147, "174785-F"));
+        var contaLucas = new ContaCorrente(2571, "174785-ZAZ");
+        var contaJoao = new ContaCorrente(1247, "174785-635");
+        listaDeContas.Adicionar(contaLucas);
+        for (int i = 0; i < listaDeContas.Tamanho; i++)
+        {
+            ContaCorrente conta = listaDeContas[i];
+            Console.WriteLine($"Indice [{i}] = {conta.Conta}/{conta.Numero_agencia}");
+        }
+
+    };
+}
+#endregion 
+#region
+List<ContaCorrente> _listaDeContas2 = new List<ContaCorrente>()
+{
+    new ContaCorrente (95,"5871774-D"){Saldo=1000},
+    new ContaCorrente(63, "36858414-E") { Saldo = 1000 },
+    new ContaCorrente(95, "2155982-F") { Saldo = 1000 }
+};
+List<ContaCorrente> _listaDeContas3 = new List<ContaCorrente>()
+{
+    new ContaCorrente (95,"5871774-G"){Saldo=1000},
+    new ContaCorrente(63, "36858414-H") { Saldo = 1000 },
+    new ContaCorrente(95, "2155982-I") { Saldo = 1000 }
+};
+
+//metodo range - ele pega uma lista e coloca no final de outra  
+_listaDeContas2.AddRange(_listaDeContas3);
+
+//for(int i = 0; i < _listaDeContas2.Count; i++)
+//{
+//    Console.WriteLine($"Indice [{i}] = conta [{_listaDeContas2[i].Conta}]");
+//}
+// 
+//metodo GetRange vai passar apenas do indice 0 a 2. Que passaria o 0 e 1 para a variavel var   
+var range = _listaDeContas2.GetRange(0, 2);
+for (int i = 0; i < range.Count; i++)
+{
+    Console.WriteLine($"Indice [{i}] = conta [{range[i].Conta}]");
+}
+
+
+#endregion
+#region 
+//ArrayList é uma tipo de coleção de objetos. Dentro dele eu coloco objetos. 
+//ArrayList _listaDeContas = new ArrayList()
+//{
+//    new ContaCorrente (95,"5871774-X"){Saldo=1000},
+//    new ContaCorrente(63, "36858414-Y") { Saldo = 1000 },
+//    new ContaCorrente(95, "2155982-Z") { Saldo = 1000 }
+//};
+//bool VerificaNomes(List<string> nomesDosEscolhidos, string escolhido)
+//{
+//    //verifica o que o ususario digitou e verifica se tem na coleção
+//    return nomesDosEscolhidos.Contains(escolhido);
+//}
+#endregion
+
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+new ByteBankAtendimento().AtendimentoCliente();
+
+
---
+++ bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,134 @@
+﻿using bytebank.Modelos.Conta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util
+{
+    public class ListaDeContasCorrentes
+    {
+        private ContaCorrente[] _itens = null;
+        private int _proximaPosicao=0;
+        //se eu não passar nenhum valor por parâmetro, vai ser 5. 
+        public ListaDeContasCorrentes(int tamanhoInicial=5)
+        {
+            _itens= new ContaCorrente[tamanhoInicial];
+
+        }
+        public void Adicionar(ContaCorrente item)
+        {
+            Console.WriteLine($"Adicionado item na posição {_proximaPosicao} ");
+            VerificarCapacidade(_proximaPosicao + 1);
+             _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        private void VerificarCapacidade(int TamanhoNecessario)
+        {
+            if (_itens.Length >= TamanhoNecessario)
+            {
+                return;
+            }
+            Console.WriteLine("Aumentando a capacidade da lista");
+            ContaCorrente[] novoArray = new ContaCorrente[TamanhoNecessario];
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                novoArray[i] = _itens[i];
+            }        
+            _itens= novoArray;  
+        }
+
+        public ContaCorrente MaiorSaldo()
+        {
+
+            ContaCorrente conta = null;
+            double maiorValor = 0;
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                if (_itens[i] != null)
+                {
+                    if (!(maiorValor > _itens[i].Saldo))
+                    {
+
+                        conta = _itens[i];
+                    }
+                }
+
+            }
+
+            return conta;
+        }
+
+        public void Remover(ContaCorrente conta)
+        {
+            int indiceItem = -1;
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                ContaCorrente contaAtual = _itens[i];
+                if(contaAtual == conta)
+                {
+                    indiceItem = i; 
+                    break;  
+                }
+            }
+            for (int i = indiceItem; i < _proximaPosicao-1; i++)
+            {
+                _itens[i] = _itens[i+1];
+            }
+            _proximaPosicao--;
+            _itens[_proximaPosicao] = null;
+
+
+        }
+
+
+        public void ExibirLista()
+        {
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                if (_itens[i] != null)
+                {
+                    var conta = _itens[i];
+                    Console.WriteLine($" Indice[{i}] = Conta:{conta.Conta} - N° da Agência: {conta.Numero_agencia}");
+                }
+            }
+        }
+
+
+        public ContaCorrente RecuperarContaPeloIndice(int indice)
+        {
+            if ((indice < 0) || (indice >= _proximaPosicao))
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+            return _itens[indice];  
+
+        }
+
+        
+
+        public int Tamanho {
+            get
+            {
+                return _proximaPosicao;
+            }
+  
+        }
+        
+        //Elemento indexavel - Pode acessar o elemento da classe como se fosse um indice 
+        public ContaCorrente this[int indice]
+        {
+            get
+            {
+                return RecuperarContaPeloIndice(indice);
+            } 
+                    
+            
+        }
+
+
+
+    }
+}
